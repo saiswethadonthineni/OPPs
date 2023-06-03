52c5b051eacc02ce7fe91ba2768aa112c32a3dca
saiswethadonthineni <donthinenisaiswetha@gmail.com>
2023-06-03 11:35:01 -0500

    first commit

--- Even.java
+++ Even.java
@@ -1,10 +1,13 @@
-public calss Even{
-  public staic void main(String args[])
-  {
-    for(i=0;i<n;i++){
-      
-    if(i%2==0){
-      System.out.println("even");
+class Even {
+
+  public static void main(String args[]) {
+    int n = 10;
+
+    for (int i = 0; i < n; i++) {
+
+      if (i % 2 == 0) {
+        System.out.println("even"+i);
       }
     }
+  }
 }
